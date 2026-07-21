9e59ac65ee3552c18e3def633adf7497ee794754
main <mehwish01012001@gmail.com>
2026-07-21 10:56:48 +0500

    d

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+import { API_BASE_URL } from '../utils/constants';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
